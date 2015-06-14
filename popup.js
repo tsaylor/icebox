d2d1ef01ed49db4d72442dfc4169c7b1185411ce
Tim Saylor <tim.saylor@gmail.com>
2015-06-14 00:41:38 -0500

    Tabs being listed through handlebars template, though randomly failing.

--- popup.js
+++ popup.js
@@ -1,10 +1,50 @@
-document.addEventListener('DOMContentLoaded', function () {
-    chrome.windows.getAll({populate: true}, function (windows) {
-        for (w in windows) {
-            document.getElementById('body').innerHTML += '<p>' + windows[w].id + '</p>';
-            for (t in windows[w].tabs) {
-                document.getElementById('body').innerHTML += '<p>&nbsp;&nbsp;&nbsp;&nbsp;' + windows[w].tabs[t].url + '</p>'
-            }
-        }
-    });
+function render(template_name, context) {
+  var iframe = document.getElementById('theFrame');
+  var message = {
+    command: 'render',
+    template_name: template_name,
+    context: context
+  };
+  iframe.contentWindow.postMessage(message, '*');
+}
+
+window.addEventListener('message', function(event) {
+  if (event.data.html) {
+    document.getElementById('body').innerHTML = event.data.html
+  } else {
+    document.getElementById('body').innerHTML = 'nodata'
+  }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.windows.getAll({populate: true}, function (windows) {
+    render('all_tabs', {windows: windows})
+  });
+});
+
+// chrome.browserAction.onClicked.addListener(function () {
+//   render('hello', {thing: 'world'})
+// });
+
+// window.onload = function () {
+//   render('hello', {thing: 'world'})
+// };
+
+// chrome.browserAction.onClicked.addListener(function() {
+//   var iframe = document.getElementById('theFrame');
+//   var message = {
+//     command: 'render',
+//     context: {thing: 'world'}
+//   };
+//   iframe.contentWindow.postMessage(message, '*');
+// });
+
+// window.addEventListener('message', function(event) {
+//   if (event.data.html) {
+//     new Notification('Templated!', {
+//       icon: 'icon.png',
+//       body: 'HTML Received for "' + event.data.name + '": `' +
+//           event.data.html + '`'
+//     });
+//   }
+// });
