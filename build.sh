8d9cde992cfa2d0bf4c8f8a9b8ae47b1a08abb89
Tim Saylor <tim.saylor@gmail.com>
2015-07-19 14:18:18 -0500

    added new files to build script

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ cp background.js _build/icebox/
 cp handlebars* _build/icebox/
 cp icebox.html _build/icebox/
 cp icebox.js _build/icebox/
+cp options.html _build/icebox/
+cp options.js _build/icebox/
 cp icon* _build/icebox/
 cp manifest.json _build/icebox/
 cp sandbox.html _build/icebox/
