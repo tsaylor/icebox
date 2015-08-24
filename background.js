13c72a0aeed991eae5b1bf0edc58214c253e42f8
Tim Saylor <tim.saylor@gmail.com>
2015-08-23 22:03:05 -0500

    Fix double install data wipe bug, and add notification when storage full.

--- background.js
+++ background.js
@@ -2,31 +2,33 @@ function onClickHandler(info, tab) {
     if (info.menuItemId == "sendtoicebox") {
         chrome.storage.sync.get('pages', function(data) {
             // add tab to storage
-            data.pages.push({
-                title: tab.title, url: tab.url, icon: tab.favIconUrl});
+            data.pages.push({title: tab.title, url: tab.url, icon: tab.favIconUrl});
             chrome.storage.sync.set({pages: data.pages}, function() {
-                // close tab
-                chrome.tabs.remove(tab.id);
-                // pop a notification
-                chrome.notifications.create('icebox-savetab', {
-                    type: "basic",
-                    message: "Saved \""+tab.title+"\"",
-                    title: "Saved Tab",
-                    iconUrl: "/icon128.png"
-                }, function(id) {return id});
+                if (chrome.runtime.lastError) {
+                    chrome.storage.sync.getBytesInUse('pages', function(count) {
+                        console.log('You are using '+count+' bytes. Google allows 8192 bytes.');
+                    });
+                    chrome.notifications.create('icebox-saveerror', {
+                        type: "basic",
+                        message: "You are out of storage space.",
+                        title: "Save Failure",
+                        iconUrl: "/icon128.png"
+                    }, function(id) {return id});
+                } else {
+                    // close tab
+                    chrome.tabs.remove(tab.id);
+                    // pop a notification
+                    chrome.notifications.create('icebox-savetab', {
+                        type: "basic",
+                        message: "Saved \""+tab.title+"\"",
+                        title: "Saved Tab",
+                        iconUrl: "/icon128.png"
+                    }, function(id) {return id});
+                }
             });
         });
     }
 };
-
-chrome.contextMenus.onClicked.addListener(onClickHandler);
-// chrome.storage.onChanged.addListener(function () {
-//     chrome.storage.sync.get('pages', function(data) {
-//         chrome.storage.sync.getBytesInUse(null, function(count){
-//             console.log("bytes in use: " + count)
-//         });
-//     });
-// })
 
 chrome.runtime.onInstalled.addListener(function(details) {
     var id = chrome.contextMenus.create({
@@ -34,12 +36,17 @@ chrome.runtime.onInstalled.addListener(function(details) {
         "contexts": ["page", "selection"],
         "id": "sendtoicebox"
     });
+    chrome.contextMenus.onClicked.addListener(onClickHandler);
 
     // data migrations
-    var previousVersion = details.reason == "install" ? "0" : details.previousVersion
-    switch (previousVersion) {
-        case "0":
-            // on fresh install, initialize the storage data
-            chrome.storage.sync.set({pages: [], settings: {storage: "chrome"}});
+    var previousVersion = details.reason == "install" ? 0 : parseInt(details.previousVersion)
+    if (previousVersion < 1) {
+        // verify sync data is empty
+        chrome.storage.sync.get(null, function(data) {
+            if(!data['pages'] && !data['settings']) {
+                // no data, initialize
+                chrome.storage.sync.set({pages: [], settings: {storage: "chrome"}});
+            }
+        });
     }
 });
